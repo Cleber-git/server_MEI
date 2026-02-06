9f1ed77987ad0ab59b4f7dcd92e6881379af551d
Cleber-git <cleber192875@gmail.com>
2026-02-05 22:03:52 -0300

    Restante dos gets post's e delete que faltavam

--- models.py
+++ models.py
@@ -25,3 +25,38 @@ class ItemVendaIn(BaseModel):
     nome: str
     valor: float
     quantidade: int
+
+class ServicoIn(BaseModel):
+    id: str
+    nome: str
+    preco: float
+    precoAnterior: float
+    dataCriacao: Optional[str] = None
+    tipo: Optional[str] = None
+
+class PerfilIn(BaseModel):
+    id: str
+    url: str
+
+class PdfVendaIn(BaseModel):
+    id: str
+    vendaId: str
+    caminhoPdf: str
+    data: Optional[str] = None
+    dataGeracao: Optional[str] = None
+    horaGeracao: Optional[str] = None
+
+
+class PagamentoIn(BaseModel):
+    id: str
+    data: str
+    valor: float
+    motivo: Optional[str] = None
+
+class DebitoClienteIn(BaseModel):
+    id: str
+    codigoCliente: str
+    periodo: str
+    valor: float
+    situacao: str
+
