2db127cece78d4c73d28f6efe2b406c692a2c8dc
Arthur-Nevess <git@github.com:Arthur-Nevess/AluGames.git>
2026-03-16 23:34:13 -0300

    Validando se email existe em base usuarioMei antes de criar usuário-> /email

--- main.py
+++ main.py
@@ -1201,11 +1201,11 @@ def validar_email(email:str):
 def receber_email(email: receiverEmail):
     """Receber o email para validar se a formatação é válida e enviar código"""
 
-    if exists("empresa", "cnpj", email.cnpj):
+    if exists("empresa", "cnpj", email.cnpj) or exists("usuariomei", "email", email.email):
         return {"sucesso": False, "mensagem": "Cnpj já foi cadastrado anteriormente"}
 # Adicionar timeOut para tirar código da fila de validade
     if not validar_email(email.email):
-        
+
         raise HTTPException(400, "Modelo não reconhecido como email")
 
     codigo = randrange(100000, 999999)
